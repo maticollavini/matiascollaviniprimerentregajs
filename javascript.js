ae8fab230eaf0ebde885781aeb3f532bcf1f5fba
matias collavini <mncollavini@gmail.com>
2023-08-18 16:57:52 -0300

    Añadí funciones

--- javascript.js
+++ javascript.js
@@ -1,6 +1,13 @@
 // Inicializar la variable que almacenará el tiempo de espera
 let tiempoDeEspera = 0;
 
+function sumaDeTiempo(nombre, i) {
+    // Actualizar el tiempo de espera sumando 10 minutos
+    tiempoDeEspera += 10;
+    // Mostrar la información del turno, nombre y tiempo de espera
+    alert("Turno N* " + i + "\nNombre: " + nombre + "\nTiempo de espera: " + tiempoDeEspera + " Minutos");
+}
+
 function turnos() {
     // Iterar desde 1 hasta 10 para simular los turnos
     for (let i = 1; i <= 10; i++) { 
@@ -12,12 +19,9 @@ function turnos() {
             // Mostrar un mensaje de alerta y salir del bucle
             alert("No ingresaste un nombre");
             break;
-        } else {
-            // Actualizar el tiempo de espera sumando 10 minutos
-            tiempoDeEspera = tiempoDeEspera + 10;
-
-            // Mostrar la información del turno, nombre y tiempo de espera
-            alert("Turno N* " + i + "\nNombre: " + nombreIngresado + "\nTiempo de espera: " + tiempoDeEspera + " Minutos");
+        } 
+        else {
+        sumaDeTiempo(nombreIngresado, i);
         }
     }
 }    
